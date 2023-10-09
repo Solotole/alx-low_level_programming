b7a6028c579149201890c32efa94d3dd20098e65
Solotole <solomontoleak47@gmail.com>
2023-10-08 19:34:01 -0700

    lowercase without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
