22017a2986aff547c3b638ff5b12b68aa189e4cc
Solotole <solomontoleak47@gmail.com>
2023-10-08 19:14:06 -0700

    printing lowercase alpahabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-
 /* more headers goes there */
- /**
-  * main - main entry point
-  *
-  * Return: always return (0)
-  */
-
+#include <stdio.h>
 /* betty style doc for function main goes there */
+/**
+ * main - main entry to a program
+ *
+ * Return: always return 0
+ */
 int main(void)
 {
-	int n, a;
+	int n;
+	int a;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-
 	a = n % 10;
-
 	if (a > 5)
 		printf("Last digit of %d is %d and is greater than 5", n, a);
-	else if (n < 6 && n != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, a);
-	else
-		printf("Last digit of %d is %d and is 0\n", n, a);
+	else if (a == 0)
+		printf("Last digit of %d is %d and is 0", n, a);
+	else if (a < 6 && a != 0)
+		("Last digit of %d is %d and is less than 6 and not 0", n, a);
+}
+	printf("\n");
+
 	return (0);
 }
+
