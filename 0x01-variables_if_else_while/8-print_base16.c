82e03f885c60ff280ef424e96794a17d19a9c73c
Solotole <solomontoleak47@gmail.com>
2023-10-08 19:45:42 -0700

    lowercase then : then numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int a;
+	char b;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a + '0');
+	for (b = 'a'; b <= 'f'; b++)
+		putchar(b);
+	putchar('\n');
+	return (0);
+}
