ee4dd886ea7f7b3586d6eb04600a0f39f6aff021
Solotole <solomontoleak47@gmail.com>
2023-10-11 19:07:05 -0700

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two values
+ * @a: first parameter
+ * @b: second parameter
+ * Return: Return (result1) sum of the 2 numbers
+ */
+
+int add(int a, int b)
+{
+	int result1;
+
+	result1 = a + b;
+	return (result1);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit -  function that prints the last digit of a number
+ * @b: number to highligt of its last digit
+ *
+ * Return: return (q) the last digit of a number
+ */
+
+int print_last_digit(int b)
+{
+	int q;
+
+	q = b % 10;
+	return (q);
+}
