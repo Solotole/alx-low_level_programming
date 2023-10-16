4d72df9058990a33f769c6f97b4a57fa6560d751
Solotole <solomontoleak47@gmail.com>
2023-10-16 07:28:20 -0700

    updating value stored in a variable

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that updates value in a variable
+ * @n: pointer parameter
+ *
+ * Return: Return void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers, aarrays and strings
