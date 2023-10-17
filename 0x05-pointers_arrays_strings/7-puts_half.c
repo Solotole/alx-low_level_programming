4a250094e54121dceb48a4a6bfec258f595bec4c
Solotole <solomontoleak47@gmail.com>
2023-10-17 09:22:02 -0700

    function printing half of the string considering odd string number

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: pointer paammeter to be printed
+ *
+ * Return: return void
+ */
+
+void puts_half(char *str)
+{
+	int i, length_of_the_string, n, j;
+
+	length_of_the_string = strlen(str);
+	n = (length_of_the_string - 1) / 2;
+	j = length_of_the_string / 2;
+	if (n % 2 == 0)
+	{
+		for (i = j; i < length_of_the_string; i++)
+			putchar(str[i]);
+	}
+	else
+	{
+		for (i = n; i < length_of_the_string; i++)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
