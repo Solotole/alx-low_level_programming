136d1aad031a77fdbda89818b4c484604b144039
Solotole <solomontoleak47@gmail.com>
2023-10-25 11:31:02 -0700

    function that prints a string then newline

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string then newline
+ * @s: paramter pointer to the string
+ *
+ * Return: return void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
