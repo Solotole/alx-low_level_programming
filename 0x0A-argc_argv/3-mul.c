ec2d910cfd51fcd313e42da156b73f87a7c9c4b7
Solotole <solomontoleak47@gmail.com>
2023-11-01 18:43:49 -0700

    function that multiplies second and third arguments passed in command line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main entry point of the program
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: return (0)
+ */
+
+int main(int argc, char **argv)
+{
+	int product, i, j;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		product = i * j;
+		printf("%d\n", product);
+	}
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
