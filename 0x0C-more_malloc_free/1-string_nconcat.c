ac9a1a20087589fa40c8f1b0f6291d4d3b16d806
Solotole <solomontoleak47@gmail.com>
2023-11-03 19:44:20 -0700

    function that concatenetes 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			ptr[len1 + i] = s2[i];
 		}
 	}
-	if (n >= len2)
+	if (n > len2 || n == len2)
 	{
 		for (i = 0; s2[i] == '\0'; i++)
 		{
