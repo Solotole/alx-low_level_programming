11ba2b165fbf221dc406fa40d1f43f73917f8de4
Solotole <solomontoleak47@gmail.com>
2023-11-06 05:18:42 -0800

    structure dog declaration

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+structurs and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - structure withstructure name dog
+ * @name: first member-dog's name
+ * @age: second member-dog's age
+ * @owner: third member-dog's owner
+ *
+ * Description: structure dog having members declared
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
