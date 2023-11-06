7d1a508e18ffbe97a604582e4f8cc1f4dcf27b48
Solotole <solomontoleak47@gmail.com>
2023-11-06 05:28:55 -0800

    structure dog declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - structure withstructure name dog
+ * struct dog - structure with structure name dog
  * @name: first member-dog's name
  * @age: second member-dog's age
  * @owner: third member-dog's owner
  *
- * Description: structure dog having members declared
+ * Description: structure dog with 3 members-name, age, and owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
