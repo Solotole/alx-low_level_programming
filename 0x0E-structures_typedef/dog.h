bdfd38cb1fe4f729c86947b7b0cd6182b98cf01c
Solotole <solomontoleak47@gmail.com>
2023-11-06 07:16:33 -0800

    accessing a structures members

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * struct dog - structure with structure name dog
  * @name: first member-dog's name
@@ -17,5 +21,6 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
