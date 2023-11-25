bb0576d9f4496c72c13e3306f16044b47dd5ad70
Solotole <solomontoleak47@gmail.com>
2023-11-24 19:53:43 -0800

    converting binary number to unsigned int-Task #0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+unsigned int power(int a, unsigned int b);
+
+#endif
