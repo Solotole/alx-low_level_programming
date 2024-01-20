0b88a056dd184a9f7aa0e75f168be5d87c2d8998
solotole <solomontoleak47@gmail.com>
2024-01-20 06:24:50 -0800

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else if ((i % 5 == 0) && (i % 3 == 0))
-			printf("FizzBuzz");
-		else
-			printf("%d", i);
-		if (i != 100)
-			printf(" ");
+		if (i < 100)
+		{
+			if (i % 15 == 0)
+				printf("FizzBuzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", i);
+		}
+		if ((i == 100) && (i % 5 == 0))
+			printf("Buzz\n");
 	}
-	printf("\n");
 	return (0);
 }
