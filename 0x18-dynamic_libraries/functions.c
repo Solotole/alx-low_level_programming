a32903d4913af54b4a686fba1839eb65ec4f2534
solotole <solomontoleak47@gmail.com>
2024-01-29 17:36:52 -0800

    calling C functions from python

---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * add - function that adds integers
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function that subtracts b from a
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return b - a
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - function that multiplis a and b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - function that divides a with b
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - function that does the modulud of two numbers
+ * @a: first paramter
+ * @b: second parameter
+ *
+ * Return: return a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
