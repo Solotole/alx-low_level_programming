c207b1b765aea03d39f72dbbbea2bd29541c38c5
solotole <solomontoleak47@gmail.com>
2024-02-16 10:14:14 -0800

    function that create hash table

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 - hash function implementating the djb2 algorithm
+ * @str: string for generating hash
  *
- * Return: hash value
+ * Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -12,8 +12,6 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
 	return (hash);
 }
