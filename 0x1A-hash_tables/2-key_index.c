66a4dc6da230c2f561181ba12a9fe3ae17ce9cae
solotole <solomontoleak47@gmail.com>
2024-02-16 12:05:52 -0800

    determining index of a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that gives you the index of a key
+ * @key: the string to be hashed(key)
+ * @size: size of the array of the hash table
+ *
+ * Return: return the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
