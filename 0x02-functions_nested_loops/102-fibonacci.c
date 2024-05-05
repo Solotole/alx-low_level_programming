1bd4229a555b05fffa446b4257bcd72129b8e3ce
solotole <solomontoleak47@gmail.com>
2024-05-05 04:11:27 -0700

    first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - main entry of the program
+ *
+ * Return: return 0 on success
+ */
+int main(void)
+{
+	int i = 0, a, b, result;
+
+	a = 1;
+	b = 2;
+	printf("%d, ", a);
+	printf("%d, ", b);
+	while (i < 48)
+	{
+		j++;
+		result = a + b;
+		if (i != 98)
+			printf("%d, ", result);
+		else if (i == 98)
+			printf("%d\n", result);
+		a = b;
+		b = result;
+		i++;
+	}
+	return (0);
+}
