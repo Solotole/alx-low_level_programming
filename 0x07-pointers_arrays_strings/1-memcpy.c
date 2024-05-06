7f760ed484daa4999f5751c162fb44e953be1cd4
solotole <solomontoleak47@gmail.com>
2024-05-06 11:44:15 -0700

    memcpy implementation

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memcpy -  function that copies memory area
+ * @dest: destination pointer
+ * @src: source destination
+ * @n: number of bytes to to copy into the destination
+ *
+ * Return: return the destination pointer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int j;
+
+	for (j = 0; src[j] != '\0' && j < n; j++)
+		*(dest + j) = src[j];
+	return (dest);
+}
