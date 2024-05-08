a557af363092646380195451f1a913e5ffdbb495
solotole <solomontoleak47@gmail.com>
2024-05-08 16:04:00 -0700

    freeing allocated memory space

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * free_grid - function to free grid
+ * @grid: pointer to the 2-D array
+ * @height: height of the grid
+ *
+ * Return: return nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+		i++;
+	}
+	free(grid);
+}
