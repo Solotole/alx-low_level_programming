c71918423fa9f44a81cb538f1a3ef5c0360cfad7
Solotole <solomontoleak47@gmail.com>
2024-05-18 15:48:37 +0100

    multiplying program arguments

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void *_calloc(unsigned int nmemb, unsigned int size);
+int main(int argc, char *argv[]);
 
 #endif
