faa6f666372f6fc0e6f9f96423de8848f653b7d9
Solotole <solomontoleak47@gmail.com>
2024-05-18 12:33:28 +0100

    calloc, malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _calloc - calloc function replica
+ * @nmemb: number of indexes of the newly allocated arrays
+ * @size: size of 1 index
+ *
+ * Return: return NULL if it fails and pointer to newly
+ * allocated array upon success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < nmemb)
+	{
+		ptr[i] = '0';
+		i++;
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
