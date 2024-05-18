58fcc0df22868f69b512cc53234319ef27db1ae0
Solotole <solomontoleak47@gmail.com>
2024-05-18 17:14:23 +0100

    creating new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,27 +10,32 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
+	int i, j, k;
 
-	if (name == NULL)
-		name = "";
-	if (owner == NULL)
-		owner = "";
 	newdog = malloc(sizeof(dog_t) * 1);
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = strdup(name);
-	if(newdog->name == NULL)
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	newdog->name = maloc(sizeof(char) * i);
+	if (newdog->name == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
 	newdog->age = age;
-	newdog->owner = strdup(owner);
+	newdog->owner = malloc(sizeof(char) * j);
 	if (newdog->owner == NULL)
 	{
 		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
+	for (k = 0; k < i; k++)
+		newdog->name[k] = name[k];
+	for (k = 0; k < j; k++)
+		newdog->owner[k] = owner[k];
 	return (newdog);
 }
