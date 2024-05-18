5f5f5e5c6e646fafb88ac8c3e5e0734c20e919cf
Solotole <solomontoleak47@gmail.com>
2024-05-18 09:42:58 +0100

    I will show how great i am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -62,7 +62,6 @@ char **strtow(char *str)
 	strings = malloc(sizeof(char *) * (words + 1));
 	if (strings == NULL)
 		return (NULL);
-	
 	for (w = 0; w < words; w++)
 	{
 		while (str[index] == ' ')
