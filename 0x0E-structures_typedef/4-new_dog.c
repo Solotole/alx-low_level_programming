0d17ac1f2f973b8334435da46ae9a322f0b71ee1
Solotole <solomontoleak47@gmail.com>
2024-05-18 17:20:30 +0100

    creating new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,7 +35,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (k = 0; k < i; k++)
 		newdog->name[k] = name[k];
+	newdog->name[k] = '\0';
 	for (k = 0; k < j; k++)
 		newdog->owner[k] = owner[k];
+	newdog->owner[k] = '\0';
 	return (newdog);
 }
