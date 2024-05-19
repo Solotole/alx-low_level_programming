2b3c103196ca58e093cfdf3125d44f9e446f175f
Solotole <solomontoleak47@gmail.com>
2024-05-19 08:35:44 +0100

    prints anything. yep!

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct print - search the func for print_all
+ * @c: character to match
+ * @f: function for that character.
+ */
+typedef struct print
+{
+	char *c;
+	void (*f)();
+} print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
