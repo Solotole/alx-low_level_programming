f0f439eef7500cbbf2858a95056b5d0dad0ea1ce
Solotole <solomontoleak47@gmail.com>
2024-05-21 07:55:03 +0100

    linear search of a sorted array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+/**
+ * linear_search - function to carry out linear search on array
+ * @array: pointer to an array to carry out linear search on
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: return -1 on failure and the index on success
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (i < size)
+	{
+		if (*(array + i) == value)
+		{
+			printf("Value checked array[%ld] = %d\n", i, array[i]);
+			return (i);
+		}
+		printf("Value checked array[%ld] = %d\n", i, array[i]);
+		i++;
+	}
+	return (-1);
+}
