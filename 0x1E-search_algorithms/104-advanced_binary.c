a49b31ab6b71150633d02119c93f17d26d788f83
Solotole <solomontoleak47@gmail.com>
2024-05-22 07:02:09 +0100

    advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,38 +1,16 @@
 #include "search_algos.h"
 /**
- * reverse_index - function responsible for finding index in the reverse
- *
- * @array: array in question
- * @top: middle - 1 of the array
- * @value: value to be searched for
- *
- * Return: return -1 on failure and the reverse index on success
- */
-int reverse_index(int *array, size_t top, int value)
-{
-	int i = (int)top;
-
-	while (i >= 0)
-	{
-		if (array[i] == value)
-			return (i);
-		i--;
-	}
-	return (-1);
-}
-/**
  * recursive_binary_search - recursive binary searching for a value
  * @array: array to search for the value
- * @start: starting index of the array searching
- * @end: ending index of the array searching
+ * @start: beginning of th search block
+ * @end: end of a search block
  * @value: value to search for
  *
  * Return: return -1 on failure and the index on success
  */
-size_t recursive_binary_search(int *array, size_t start, size_t end, int value)
+int recursive_binary_search(int *array, size_t start, size_t end, int value)
 {
 	size_t middle, i;
-	int reverse;
 
 	if (start > end)
 		return (-1);
@@ -47,16 +25,14 @@ size_t recursive_binary_search(int *array, size_t start, size_t end, int value)
 	middle = start + (end - start) / 2;
 	if (array[middle] == value)
 	{
-		reverse = reverse_index(array, middle - 1, value);
-		if (reverse >= 0)
-			return (recursive_binary_search(array, start, middle, value));
-		else if (reverse == -1)
+		if (middle == start || array[middle - 1] < value)
 			return (middle);
+		return (recursive_binary_search(array, start, middle, value));
 	}
-	if (value > array[middle])
+	if (array[middle] > value)
+		return (recursive_binary_search(array, start, end - 1, value));
+	if (array[middle] < value)
 		return (recursive_binary_search(array, middle + 1, end, value));
-	if (value < array[middle])
-		return (recursive_binary_search(array, start, middle - 1, value));
 	return (-1);
 }
 /**
@@ -72,8 +48,8 @@ int advanced_binary(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 	index = recursive_binary_search(array, 0, size - 1, value);
-	return ((int)index);
+	return ((size_t)index);
 }
